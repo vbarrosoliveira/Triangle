8e9f451508b760b7c8feedf98350468461337b81
Vanessa Barros de Oliveira <vbarrosoliveira@gmail.com>
2023-02-22 16:17:59 +0100

    Update Maint.java
    added it_is_a_triangle method, improved triangle_class method, addes if/else in main method

--- Maint.java
+++ Maint.java
@@ -1,5 +1,5 @@
 package com.trianglearea;
-import java.lang.Math;
+
 import java.util.Scanner;
 //A Java method to calculate the area  of a triangle classifying them as:
 //equilateral,isosceles,scalene,right
@@ -16,33 +16,51 @@ public class Maint {
         double side=myObj.nextDouble();
         double side1=myObj.nextDouble();
         double side2=myObj.nextDouble();
-       System.out.println("Area:" + triangle_area(side,side1,side2));
-        triangle_class(side, side1, side2);
-    }
-
-    private static void triangle_class(double side, double side1, double side2) {
-
-        if (side == side1 && side1 == side2) {
-
-            System.out.println("this triangle is equilateral");
-        } else if (side == side1  ||  side1 == side2) {
-            System.out.println("this triangle is isosceles");
-
-        } else {
-            System.out.println("this triangle is scalene");
+        boolean IsItaTriagle=it_is_a_triangle(side, side1, side2);
+        if (IsItaTriagle){
+            System.out.println("Area:" + triangle_area(side, side1, side2));
+            System.out.println("Classification:" + triangle_class(side, side1, side2));
+        }
+        else {System.out.println("Are you sure you entered a valid input?");
+            System.out.println("1.each side must be a positive number");
+            System.out.println("2.the sum of the lengths of any two sides of a triangle must be greater than to the length of the third side");
 
         }
+    }
+
+     private static String triangle_class ( double side, double side1, double side2){
+
+            if (side == side1 && side1 == side2) {
+
+                return "equilateral";
+            } else if (side == side1 || side1 == side2) {
+                return "isosceles";
+
+            } else {
+                return "scalene";
+
+            }
 
 
 
     }
 
-    private static double triangle_area(double side, double side1, double side2) {
-        double semi_perimeter=(side+side1+side2)/2;
-        double inside_the_root= semi_perimeter*(semi_perimeter-side)*(semi_perimeter-side1)*(semi_perimeter-side2);
-        double area= Math.sqrt(inside_the_root);
-        return area;
-    }
+   private static String triangle_area ( double side, double side1, double side2){
+            double semi_perimeter = (side + side1 + side2) / 2;
+            double inside_the_root = semi_perimeter * (semi_perimeter - side) * (semi_perimeter - side1) * (semi_perimeter - side2);
+            double area = Math.sqrt(inside_the_root);
 
+
+            return String.format("%.2f", area);
+        }
+    
+    private static boolean it_is_a_triangle(double side, double side1, double side2) {
+        boolean TriangleIneq = side + side1 > side2 && side2 + side1 > side && side + side2 > side1;
+        boolean PositiveSides=side>0 && side1 >0 && side2>0;
+
+        if((TriangleIneq && PositiveSides))
+        return TriangleIneq;
+        else return false;
+    }
 
 }
